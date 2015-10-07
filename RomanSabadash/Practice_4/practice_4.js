a51609afe234708ba1f2c7e5e9c55dcd9413e157
Roman Sabadash <r.v.sabadash@gmail.com>
2015-10-07 13:13:20 +0300

    Practice Work

---
+++ RomanSabadash/Practice_4/practice_4.js
@@ -0,0 +1,19 @@
+var a;
+console.log(a);
+
+var b;
+	b = true;
+console.log(b);
+	
+var c = 75;
+console.log(c);
+	
+var d;
+	d = "First String";
+console.log(d);
+	
+var e = null;
+console.log(e);
+
+var f = null;
+console.log(typeof f);
