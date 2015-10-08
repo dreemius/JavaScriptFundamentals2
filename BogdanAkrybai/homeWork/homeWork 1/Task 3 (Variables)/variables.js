b197f9251fce36cf67e4a6a5c20d505fb29d05f7
pro360ua <pro360ua@gmail.com>
2015-10-08 14:08:50 +0300

    hw1

---
+++ BogdanAkrybai/homeWork/homeWork 1/Task 3 (Variables)/variables.js
@@ -0,0 +1,10 @@
+var randomNum = 50;
+var randomNum2 = 50;
+var randomNum3 = 50;
+console.log(randomNum, randomNum2, randomNum3);
+
+
+var name = "Bogdan";
+var age = 22;
+var city = "Chernivtsi";
+console.log(name, age, city);
---
+++ BogdanAkrybai/homeWork/homeWork 1/Task 4 (Data Types)/types.js
@@ -0,0 +1,10 @@
+var myValue;
+console.log(myValue);
+
+var myStatement = true,
+	myAge = 22,
+	myName = "Bogdan",
+	myQuantity = null;
+console.log(myStatement, myAge, myName, myQuantity);
+typeof null;
+console.log(null);
