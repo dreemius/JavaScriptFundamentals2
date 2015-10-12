0e01330e32e16005c8cc998772e351ceb74153ff
AlexHristodul <AlexHristodul@gmail.com>
2015-10-12 15:35:33 +0300

    Update JavaScript.js

--- AlexHristodul/Lessons/Lesson 3/JavaScript.js
+++ AlexHristodul/Lessons/Lesson 3/JavaScript.js
@@ -1,19 +1,25 @@
-var sum = 0;
+var totalSum = 0;
+var i = 0;
 
-for (var i = 0; i < 15; i++) {
-	var first = Math.floor((Math.random() * 6) + 1); 
-	var second = Math.floor((Math.random() * 6) + 1);
-	document.getElementById("result").innerHTML += "Первая кость: " + first + "   Вторая кость: " + second;
-	document.getElementById("result").innerHTML += first == second ? " Выпал дубль. Число - "+ first : "<br>";
+document.getElementById("result").innerHTML += "<h3>START</h3>";
 
-	if ((first == second) && (first == 1 || first == 6)) {
-		document.getElementById("result").innerHTML += " - Две " + (first == 6 ? " шестёрки<br>" : " единицы<br>");
-	} else if (first == second) {
-		document.getElementById("result").innerHTML += "<br>";
+for (i=0;i<15;i++) {
+	
+	var first = Math.floor ((Math.random() * 6) + 1);
+	var second = Math.floor ((Math.random() * 6) + 1);
+	
+	document.getElementById("result").innerHTML += "First bones: - " + first + " Second bones: - " + second + "<br>";
+	
+	if (first == second) {
+		document.getElementById("result").innerHTML += "Rolls doubles. Number - " + first + "<br>";
 	}
-
-	sum += first + second;
-
-};
-
-document.getElementById("result").innerHTML += sum > 100 ? "Победа, вы набрали " + sum + " очков" : "Проигрыш, у вас " + sum + " очков";
+	if (first == 1 && second == 1) {
+		document.getElementById("result").innerHTML += "<i>-Two units-</i><br>";
+	}
+	if (first == 6 && second == 6) {
+		document.getElementById("result").innerHTML += "<i>-Two sixes-</i><br>";
+	}
+	totalSum += first + second;
+}
+document.getElementById("result").innerHTML += "<br>";
+document.getElementById("result").innerHTML += totalSum < 100 ? "<b>You LOSE - :(<br>Your result is: </b>" + totalSum + ".": "<b>You WIN - :)<br>Your result is: </b>" + totalSum + ".";
