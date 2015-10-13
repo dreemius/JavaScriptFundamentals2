3519180a08c90c06d6940d54fa79ce9ae6f4c959
Eugene <dreemius@gmail.com>
2015-10-13 20:45:27 +0300

    code review

--- VildanovVladislav/lesson 3/Homework.js
+++ VildanovVladislav/lesson 3/Homework.js
@@ -1,19 +1,20 @@
-var total=0
+var total= 0,
+	resultText = document.getElementById("result");
+
 for (var i=0; i<15; i++) {
     var first = Math.floor((Math.random() * 6) + 1); 
     var second = Math.floor((Math.random() * 6) + 1);
+	resultText.innerHTML += "Первая кость:" + first + "Вторая кость:" + second + "<br>";
+
   	if(first==second) {
-  		document.getElementById("result").innerHTML += "Выпал дубль. Число" + first + "<br>";
-  	} else {
-  		document.getElementById("result").innerHTML += "Первая кость:" + first + "Вторая кость:" + second + "<br>";
+  		resultText.innerHTML += "Выпал дубль. Число" + first + "<br>";
+		if (first==1 || first==6) {
+			resultText.innerHTML += "Две" + (first==1) ? "еденицы " : "шестерки ";
+			resultText.innerHTML += "<br>";
+		}
   	}
-  	if (first==1 && second==1) {
-		document.getElementById("result").innerHTML += "Две еденицы <br>";
-	} if(first==6 && second==6) {
-		document.getElementById("result").innerHTML += "Две шестёрки <br>";
-	}
 	total+=first+second;
-		document.getElementById("result").innerHTML += "Сума" + total + "<br>";
 }
 
-document.getElementById("result").innerHTML += (total>=100) ? "Победа, вы набрали" + total + "очков" : "Вы проиграли, у вас" + total + "очков" + "<br>";
+resultText.innerHTML += "Сума" + total + "<br>";
+resultText.innerHTML += (total>=100) ? "Победа, вы набрали" + total + "очков" : "Вы проиграли, у вас" + total + "очков" + "<br>";
