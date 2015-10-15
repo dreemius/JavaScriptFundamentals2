1535e1ff28a123dbddaaec06bd50d56ea51d76ee
OlegOD <eRR0R4ik@yandex.ru>
2015-10-15 21:07:10 +0300

    work2

---
+++ OlegOD/Lesson_2/Task_3/T3.js
@@ -0,0 +1,9 @@
+var a=x=z=70;
+
+console.log('a=x=z='+a);
+a="strings";
+x=666;
+z=true;
+console.log ('a='+a);
+console.log ('x='+x);
+console.log ('z='+z);
---
+++ OlegOD/Lesson_2/Task_6/T6.js
@@ -0,0 +1,20 @@
+
+var a;
+
+console.log('a='+a);
+console.log("**************");
+console.log(String(a));
+console.log(Number(a));
+console.log(Boolean(a));
+console.log("**************");
+a=1;
+console.log(String(a));
+console.log(Number(a));
+console.log(Boolean(a));
+console.log(!(a));
+console.log("**************");
+a="50cent";
+console.log('a='+a);
+console.log(Number(a));
+console.log(parseInt(a));
+console.log(Boolean(a));
