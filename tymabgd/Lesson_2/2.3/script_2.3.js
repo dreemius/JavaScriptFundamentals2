242e26aed733ac76c10852e7bcd539f04249ed08
tymabgd <tyma.bgd@gmail.com>
2015-10-20 02:41:54 +0300

    20.10.205

---
+++ tymabgd/Lesson_2/2.3/script_2.3.js
@@ -0,0 +1,10 @@
+/*
+    Create 10/20/2015.
+ */
+
+var per1 = 1, per2 = 1, per3 = 1;
+    console.log(per1, per2, per3);
+
+var per1 = 2, per2 = "Hello", per3 = false;
+    console.log(per1, per2, per3);
+
