0791ec0d07f65ef38f15de839b858e857669d8d8
Hash <hash.cv@gmail.com>
2015-10-19 22:57:50 +0300

    Changes for issue on Github

--- ValentynTymku/Lesson_3/script.js
+++ ValentynTymku/Lesson_3/script.js
@@ -3,17 +3,17 @@ var total = 0;
 for (var i = 0; i < 15; i++) {
 	var first = Math.floor((Math.random() * 6) + 1); 
 	var second = Math.floor((Math.random() * 6) + 1);
+	total += first + second;
 	document.getElementById("result").innerHTML += "Первая кость:" + first + " Вторая кость:" + second;
-	document.getElementById("result").innerHTML += first == second ? " Выпал дубль. Число "+ first : "<br>";
 
-	if ((first == second) && (first == 1 || first == 6)) {
-		document.getElementById("result").innerHTML += " Две " + (first == 6 ? " шестерки<br>" : " единицы<br>");
-	} else if (first == second) {
-		document.getElementById("result").innerHTML += "<br>";
+	if (first == second) {
+		document.getElementById("result").innerHTML += " Выпал дубль. Число "+ first;
+		if (first == 1 || first == 6) {
+			document.getElementById("result").innerHTML += " Две " + (first == 6 ? "шестерки" : "единицы");
+		};
 	}
 
-	total += first + second;
-
+	document.getElementById("result").innerHTML += "<br>"
 };
 
 document.getElementById("result").innerHTML += total > 100 ? "Победа, вы набрали " + total + " очков" : "Вы проиграми, у вас " + total + " очков";
