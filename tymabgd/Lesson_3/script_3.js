df55084bfc8745f3bd84f70cad22b1be4c488e6e
tymabgd <tyma.bgd@gmail.com>
2015-10-21 20:09:52 +0300

    Lesson 2 and 3 - mission accomplished

--- tymabgd/Lesson_3/script_3.js
+++ tymabgd/Lesson_3/script_3.js
@@ -2,11 +2,21 @@
 "Две шестерки"
 "Две единицы"
  */
+var total = 0;
+
 for (var i = 1; i <= 15; i++){
-    var first = Math.floor((Math.random() * 6) + 1);
-    var second = Math.floor((Math.random() * 6) + 1);
-        document.getElementById("result").innerHTML += "Первая кость: " + first + " Вторая кость: " + second + "<br>";
-            if (first == second) {
-                document.getElementById("result").innerHTML += "Выпал дубль. Число:" + first + "<br>";
-            }
+    var first = Math.floor((Math.random() * 6) + 1),
+        second = Math.floor((Math.random() * 6) + 1),
+        temp;
+
+    document.getElementById("result").innerHTML += "Первая кость: " + first + " Вторая кость: " + second + "<br>";
+
+    if (first == second && (first == 1 || second == 6) ) {
+        temp = first;
+        document.getElementById("result").innerHTML += "Выпал дубль. Число: " + temp + "<br>";
+    }
+
+    total += first + second;
 }
+(total > 100) ? document.getElementById("result").innerHTML += "Победа, вы набрали " + total + " очков."
+               :document.getElementById("result").innerHTML += "Вы проиграми, у вас " + total + " очков.";
