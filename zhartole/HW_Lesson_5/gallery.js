ce531459594c09c31d423ffb732481b369895451
zhartole <zhartole@gmail.com>
2015-10-26 22:55:37 +0200

    HW not so good to check)

--- zhartole/HW_Lesson_5/gallery.js
+++ zhartole/HW_Lesson_5/gallery.js
@@ -1,7 +1,7 @@
-var NUMBERS_TO_DISPLAY = 0; // if 0 - all, if 1 - odd, if 2 - even
-var resultContainer = $('#result');
-var resultHTML = "";
-var itemTemplate = '<div class="col-sm-3 col-xs-6">\
+var NUMBERS_TO_DISPLAY = 0, // if 0 - all, if 1 - odd, if 2 - even
+    resultContainer = $('#result'),
+    resultHTML = "",
+    itemTemplate = '<div class="col-sm-3 col-xs-6">\
 				<img src="$url" alt="$name" class="img-thumbnail">\
 				<div class="info-wrapper">\
 					<div class="text-muted">$number: $name</div>\
@@ -10,89 +10,18 @@ var itemTemplate = '<div class="col-sm-3 col-xs-6">\
 				</div>\
 			</div>';
 
-// Это реальный объект на основе которого вам надо дудет строить галлерею
-var data = [{
-	url: "http://desktopwallpapers.org.ua/mini/201507/40069.jpg",
-	name: "МАШИНКА",
-	id : 1,
-	description : " be conveyed to users of assistive technologies – such as",
-	date : 1422153200637
-},{
-	url: "http://desktopwallpapers.org.ua/mini/201507/40068.jpg",
-	name: "ДРУГАЯ МАШИНКА",
-	id : 2,
-	description : "sing color to add meaning to a button",
-	date : 1421153200637
-},{
-	url: "http://desktopwallpapers.org.ua/mini/201507/40067.jpg",
-	name: "НАЗВАНИЕ",
-	id : 3,
-	description : " be conveyed to users of assistive technologies",
-	date : 1426153200637
-},{
-	url: "http://desktopwallpapers.org.ua/mini/201507/40066.jpg",
-	name: "ОПИСАНИЕ",
-	id : 4,
-	description : "ssistive technologies – such as screen readers. Ensure",
-	date : 1428153200637
-},{
-	url: "http://desktopwallpapers.org.ua/mini/201507/40069.jpg",
-	name: "ИМЯ МАШИНЫ",
-	id : 5,
-	description : "color to add meaning to a button only provides",
-	date : 1402153200637
-},{
-	url: "http://desktopwallpapers.org.ua/mini/201507/40064.jpg",
-	name: "МЕРСЕДЕС",
-	id : 6,
-	description : "om the content itself (the visible text of the button)",
-	date : 1442153200637
-},{
-	url: "http://desktopwallpapers.org.ua/mini/201507/40063.jpg",
-	name: "БМВ",
-	id : 7,
-	description : "r is either obvious from the content itself",
-	date : 1482153200637
-},{
-	url: "http://desktopwallpapers.org.ua/mini/201507/40062.jpg",
-	name: "ЛАДА КАЛИНА",
-	id : 8,
-	description : "included through alternative means, such as additional text hidden with the",
-	date : 1442153200637
-},{
-	url: "http://desktopwallpapers.org.ua/mini/201507/40069.jpg",
-	name: "ДЖИП",
-	id : 9,
-	description : " meaning to a button only provides a visual",
-	date : 1322153200637
-},{
-	url: "http://desktopwallpapers.org.ua/mini/201507/40069.jpg",
-	name: "ДАЧА ЛОГАН",
-	id : 10,
-	description : "uded through alternative means, such as additional",
-	date : 1322159200637
-}];
-
-
 function extractDataToDisplay() {
 	return data.filter(function(item) {
-		if(NUMBERS_TO_DISPLAY == 1) {
-			return item.id % 2 != 0;
-		} else if(NUMBERS_TO_DISPLAY == 2) {
-			return item.id % 2 == 0;
-		} else return data;
+		return  NUMBERS_TO_DISPLAY == 1 ? item.id % 2 != 0 :(NUMBERS_TO_DISPLAY == 2 ? item.id % 2 == 0 : data);
 	});
 }
 
+extractDataToDisplay().forEach(function(item) {
 
-extractDataToDisplay().forEach(function(item, i, arg) {
-
-	var capitalizedFirstLetter = item.name.charAt(0) +
-		item.name.slice(1).toLowerCase();
-	var shortDescription = item.description.slice(0, 15);
-	var date = new Date(item.date);
-	var formattedDate = date.getFullYear() + "/" + (+date.getMonth() + 1)
-		+ "/" + date.getDate();
+	var capitalizedFirstLetter = item.name.charAt(0) + item.name.slice(1).toLowerCase(),
+	    shortDescription = item.description.slice(0, 15),
+	    date = new Date(item.date),
+	    formattedDate = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate()+ " " + date.getHours() + ":" + date.getMinutes();
 	resultHTML += itemTemplate
 		.replace("$number", item.id)
 		.replace(/\$name/gi, capitalizedFirstLetter)
@@ -101,5 +30,9 @@ extractDataToDisplay().forEach(function(item, i, arg) {
 		.replace("$date", formattedDate);
 });
 
-
 resultContainer.html(resultHTML);
+
+
+//var NUMBERS_TO_DISPLAY = $('change-image').click(function(){
+//	return NUMBERS_TO_DISPLAY == 0 || NUMBERS_TO_DISPLAY == 2  ? NUMBERS_TO_DISPLAY == 1 : NUMBERS_TO_DISPLAY == 0;
+// }),
