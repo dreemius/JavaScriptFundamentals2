35f06f8390a6ff4e3399a72a39f4786942e66ffa
SheffBRC <sheff88@bk.ru>
2015-10-26 10:47:58 +0200

    Update script.js

--- VadymShevchuk/Lesson4/script.js
+++ VadymShevchuk/Lesson4/script.js
@@ -1,19 +1,10 @@
-var 
-i=1; 
-total=0;
-first=0;
-second=0;
-function getRndNumber()
-{
-	first = Math.floor((Math.random() * 6) + 1); 
-	second = Math.floor((Math.random() * 6) + 1); 
-}
-function print(str)
-{
+var i=1, total=0, first=0, second=0;
+function getRndNumber(){
+	return Math.floor((Math.random() * 6) + 1); 
+}function print(str){
 	document.getElementById("result").innerHTML +=str;
 }
-function special(first,second)
-{
+function special(first,second){
 	if (first == second)
 	print("Выпал дубль: <b>" + first + "</b><br>");
 	if (first==1 && second==1)
@@ -21,20 +12,17 @@ function special(first,second)
 	if (first==6 && second==6)
 	print("<i>Две шестерки</i><br>");	
 }
-function totalCount(first,second)
-{
+function totalCount(first,second){
 	total=total+first+second;
 }
-function totalPrint(total)
-{
-document.getElementById("result").innerHTML += "<br><b>Ваш результат: " + total + "</b><br>";
-document.getElementById("result").innerHTML += total > 100 ? "Победа, вы набрали " + total + " очков" : "Вы проиграми, у вас " + total + " очков";
+function totalPrint(total){
+print("<br><b>Ваш результат: " + total + "</b><br>");
+print( total > 100 ? "Победа, вы набрали " + total + " очков" : "Вы проиграми, у вас " + total + " очков");
 }
-function run()
-{
-for (i=1; i<=15; i++)
-{
-	getRndNumber();
+function run(){
+for (i=1; i<=15; i++){
+	first=getRndNumber();
+	second=getRndNumber();
 	print("Первая кость: " + first + "; Вторая кость: " + second + "<br>");
 	special(first,second);
 	totalCount(first,second);
@@ -42,11 +30,3 @@ for (i=1; i<=15; i++)
 }
 run();
 totalPrint(total);
-
-/*
-function getRndNumber()
-{
-	return Math.floor((Math.random() * 6) + 1); 
-}
-first= getRndNumber; ???
-*/
