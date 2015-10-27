0b65fd78e6cb14467d639254c1103fdca34b9b78
hiswa94 <hiswa2015@gmail.com>
2015-10-27 21:12:50 +0200

    fixing )

--- HavrylyukIgor/Lessons/Lesson3HW/bones.js
+++ HavrylyukIgor/Lessons/Lesson3HW/bones.js
@@ -1,17 +1,17 @@
  var total = 0,
-     print=document.getElementById("result").innerHTML;
+     print=document.getElementById("result");
 
    for (var i = 1; i <= 15; i++){
  var first = Math.floor((Math.random() * 6) + 1),
      second = Math.floor((Math.random() * 6) + 1);
 
-    print+= "Первая кость: " + first + " Вторая кость: " + second + "<br>";
+    print.innerHTML+= "Первая кость: " + first + " Вторая кость: " + second + "<br>";
 
     if (first == second && (first == 1 || second == 6) ) {
-        print+= "Выпал дубль. Число: " + first + "<br>";
+        print.innerHTML+= "Выпал дубль. Число: " + first + "<br>";
     }
 
     total += first + second;
 }
-(total > 100) ? print+= "Победа, вы набрали " - total - " очков."
-               :print+= "Вы проиграми, у вас " - total - " очков.";
+(total > 100) ? print.innerHTML+= "Победа, вы набрали " + total + " очков."
+               :print.innerHTML+= "Вы проиграми, у вас " + total + " очков.";
