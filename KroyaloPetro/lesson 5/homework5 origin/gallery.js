279b7c6f381febba3a961bd312fb17c2a45a8728
fredis0629 <fredis0629@mail.ru>
2015-10-28 21:28:59 +0400

    close issue for Homework to lesson 5

--- KroyaloPetro/lesson 5/homework5 origin/gallery.js
+++ KroyaloPetro/lesson 5/homework5 origin/gallery.js
@@ -1,10 +1,11 @@
-
-var resultContainer = $('#result');
-var resultHTML 		= "";
-var MAX 			= data.length,
-	STARTP			= 1;
-	ENDP			= data.length;
-var itemTemplate = '<div class="col-sm-3 col-xs-6">\
+run();
+function run(){
+	var resultHTML 		= '<div class="starter-template">\
+			<h1>JS Fundamentals first demo page</h1>\
+			<p class="lead">Simple galley builder</p>\
+		</div>';
+	var buffHTML		= "";
+	var itemTemplate    = '<div class="col-sm-3 col-xs-6">\
 				<img src="$url" alt="$name" class="img-thumbnail">\
 				<div class="info-wrapper">\
 					<div class="text-muted">$number: $name</div>\
@@ -12,23 +13,45 @@ var itemTemplate = '<div class="col-sm-3 col-xs-6">\
 					<div class="text-muted">$date</div>\
 				</div>\
 			</div>';
-function corectingName(namec){
-	return namec[0].toLocaleUpperCase()+namec.slice(1).toLocaleLowerCase();
+	var MAX_DISPLAY		= data.length,
+		START_P			= 0,
+		END_P			= data.length,
+		buffForDivRow	= 0;
+	(MAX_DISPLAY < (END_P-START_P)) ? END_P = START_P + MAX_DISPLAY : NaN;
+	data.slice(START_P,END_P).forEach(function (item, index){
+		if (isAvailableRange(index, START_P, END_P)){
+			buffHTML += itemTemplate
+				.replace("$number", item.id)
+				.replace(/\$name/gi, correctName(item.name))
+				.replace("$url", item.url)
+				.replace("$description", cutDescription(item.description))
+				.replace("$date", formateDate(new Date(item.date)));
+			buffForDivRow++;
+			if (isPrint(buffForDivRow, index, START_P, END_P)){
+				buffForDivRow 	= 0;
+				resultHTML	   += '<div class="row">'+buffHTML+'</div>';
+				buffHTML	  	= "";
+			}
+		}
+	});
+	printInHTML('<div>'+resultHTML+'</div>')
 }
-function cutFromDescription(str){
-	return str.slice(0,15);
+function printInHTML(stringToHTML){
+	var resultContainer = $('#container');
+	resultContainer.html(stringToHTML);
 }
-function formateDate(datef){
-	return datef.getFullYear()+'/'+datef.getMonth()+'/'+datef.getDate()+' '+datef.getHours()+':'+datef.getMinutes();
+function isPrint(buffForDivRow, index, START_P, END_P){
+	return ((buffForDivRow == 4)||((index+1) == (END_P-START_P)))
 }
-data.forEach(function(item, index){
-if ((index < MAX)&&(index >= STARTP-1)&&(index < ENDP)){
-resultHTML += itemTemplate
-	.replace("$number", item.id)
-	.replace(/\$name/gi, corectingName(item.name))
-	.replace("$url", item.url)
-	.replace("$description", cutFromDescription(item.description))
-	.replace("$date", formateDate(new Date(item.date)));
+function isAvailableRange(index, START_P, END_P){
+	return (index < (END_P - START_P))
 }
-});			
-resultContainer.html(resultHTML);
+function correctName(name){
+	return name[0].toLocaleUpperCase()+name.slice(1).toLocaleLowerCase();
+}
+function cutDescription(description){
+	return description.slice(0,15);
+}
+function formateDate(date){
+	return date.getFullYear()+'/'+(date.getMonth()+1)+'/'+date.getDate()+' '+date.getHours()+':'+date.getMinutes();
+}
