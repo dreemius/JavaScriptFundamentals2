95b4748448bd2815c03d62db62ca77bceb99940c
OlegOD <eRR0R4ik@yandex.ru>
2015-11-08 23:37:35 +0200

    check 5 plz

---
+++ OlegOdoviichuk/Lesson_5/homework5/task_5.js
@@ -0,0 +1,57 @@
+var ELEMENTS_NUMBER = 4;
+var ELEMENTS_START = 5;
+var ELEMENTS_FINISH = 10;
+var newArr = [];
+
+newArr = data.filter(function(item) {
+	return (item.id >= ELEMENTS_START && item.id <= ELEMENTS_FINISH);
+	
+});
+
+
+function changeName(name) {
+	return name[0].toUpperCase() + name.slice(1).toLowerCase();
+};
+function sliceDescription(description) {
+	return description.slice(0, 15);
+};
+
+function createDateObject(date){
+    var tmpDate = new Date(date);
+    return tmpDate.getFullYear() + "/" +
+	tmpDate.getMonth() + "/" +
+	tmpDate.getDate() + " " +
+	tmpDate.getHours() + ":" +
+	tmpDate.getMinutes();
+};
+for(var i = 0; i < newArr.length;i++) {
+	
+	newArr[i].name = changeName(newArr[i].name);
+	newArr[i].description = sliceDescription(newArr[i].description);
+	newArr[i].date = createDateObject(newArr[i].date);
+	
+}
+var resultContainer = $('#result');
+var resultHTML = "";
+var itemTemplate = '<div class="col-sm-3 col-xs-6">\
+<img src="$url" alt="$name" class="img-thumbnail">\
+<div class="info-wrapper">\
+<div class="text-muted">$number: $name</div>\
+<div class="text-muted">$description</div>\
+<div class="text-muted">$date</div>\
+</div>\
+</div>';
+
+function printResult(arrObj){
+	for (var j=0; j<arrObj.length;j++){
+		resultHTML += itemTemplate
+		.replace("$number", arrObj[j].id)
+		.replace(/\$name/gi, arrObj[j].name)
+		.replace("$url", arrObj[j].url)
+		.replace("$description",arrObj[j].description)
+		.replace("$date", arrObj[j].date);	
+	}
+	return resultContainer.html(resultHTML);
+}
+
+printResult(newArr);
