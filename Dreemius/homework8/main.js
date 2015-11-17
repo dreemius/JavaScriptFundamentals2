4accc67a84373a8de3dc42c73532fc93195d8aa1
Eugene <dreemius@gmail.com>
2015-11-17 20:46:53 +0200

    Lesson 8 improvements

--- Dreemius/homework8/main.js
+++ Dreemius/homework8/main.js
@@ -2,22 +2,61 @@
 
 var formValidator = (function(){
 	
+	var DOMElements = null;
+	
+	function showErrorMsg(){}
+	function hideErrorMsg(){}
+	
+	function showSuccessMsg(){}
+	
+	function showTable(){}
+	function hideTable(){}
+	
+	
+	function addNewTableLine() {
+		var row;
+		
+		
+		DOMElements.tableContent.innerHTML += row
+		hideErrorMsg();
+		showTable();
+	}
+	
+	
+	function checkForm () {
+		if(DOMElements.name.value 
+			&& DOMElements.email.value 
+			&& DOMElements.password.value) {			
+				showTable();
+			
+		} else {
+			showErrorMsg();
+		}
+	}
+	
+	DOMElements.submitBtn.addEventListener(checkForm);
+	
+	
 	validateForm : function(){},
 	reset : function(){},
 	
 	return {
-		setForm : function(form){},
-		initValidator: function(form){}
+		setForm : function(form){	
+			DOMElements = form;
+		},
+		initValidator: function(form){
+			validateForm();
+		}
 	}
 	
 	
 }())
 
 formValidator.setForm({
-	name : document.querySelector("inputName"),
-	imail : document.querySelector("inputEmail"),
-	....
-	tableWrapper
+	name 	: document.querySelector("inputName"),
+	email 	: document.querySelector("inputEmail"),
+	password : document.querySelector("inputPassword"),
+	tableContent : document.querySelector("table-content"),
 	submitBtn
 	resetBtn
 })
