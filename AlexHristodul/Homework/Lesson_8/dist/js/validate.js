3f96b12c8eb7d3f85e4ec07c3014557c6478b55e
AlexHristodul <AlexHristodul@gmail.com>
2015-11-21 21:15:48 +0200

    Homework for lesson_8

--- AlexHristodul/Homework/Lesson_8/dist/js/validate.js
+++ AlexHristodul/Homework/Lesson_8/dist/js/validate.js
@@ -1,24 +1,66 @@
 'use strict';
 // Created by iPhoenix© development 21.11.2015
 //********************************************
+
 var formValidator = (function(){
 	var input = null;
 
-	function showDiv(selector) {
+	function showBanner(selector) {
 		$(selector).show(2000, function() {
 			$(this).hide(4000);
 		});
 	}
 	
+	function createTable(paramSet) {
+		var element = paramSet.el.appendChild(document.createElement(paramSet.type));
+		paramSet.innerHTML && (element.innerHTML = paramSet.innerHTML);
+		return element;
+	}
+	
+	function addRow() {
+		var count = 2;
+		var firstRow = document.querySelector('#table-content');
+		var rowCreate = createTable({
+				el: firstRow,
+				type: 'tr',
+		});		
+		var zeroCell = createTable({
+				el: rowCreate,
+				type: 'th',
+				innerHTML: count++
+		});
+		var firstCell = createTable({
+				el: rowCreate,
+				type: 'td',
+				innerHTML: input.name.value
+		});
+		var secondCell = createTable({
+				el: rowCreate,
+				type: "td",
+				innerHTML: input.email.value
+		});
+		var thirdCell = createTable({
+				el: rowCreate,
+				type: "td",
+				innerHTML: input.pasword.value.replace(/[\s\S]/g, "*")
+		});
+	}
+	
 	function checkForm() {
+	
+		function isValidEmail(myEmail) { 
+			var reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/;
+			return reg.test(myEmail); 
+} 
 		if(input.name.value
-		   && input.email.value
+		   && isValidEmail(input.email.value)
 		   && input.pasword.value) {
-			$('.form-group').hide();
-			showDiv('#success-msg');
+			$('.form-horizontal').hide();
+			showBanner('#success-msg');
 			$('#tableOfResult').show();
+			addRow();
 		} else {
-			showDiv('#error-msg');
+			showBanner('#error-msg');
 		}
 	}
 	
