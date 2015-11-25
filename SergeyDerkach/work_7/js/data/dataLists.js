0d6c4e8dcdd5d18ba3cfa373ef0b2cfd8ba66161
Eugene <dreemius@gmail.com>
2015-11-25 20:15:25 +0200

    Refactored homework7 for SergeyDerkach

---
+++ SergeyDerkach/work_7/js/data/dataLists.js
@@ -0,0 +1,489 @@
+/**
+ * Created by krepysh on 27.10.2015.
+ */
+var dataLists = {
+    myHobbi : [{
+        url: "gallary/MyHobbi/_kGv1HzaDb4.jpg",
+        name: "МАШИНКА",
+        id : 1,
+        description : " be conveyed to users of assistive technologies – such as",
+        date : 1422153200637
+    },{
+        url: "gallary/MyHobbi/9yZUz5zjRA0.jpg",
+        name: "ДРУГАЯ МАШИНКА",
+        id : 2,
+        description : "sing color to add meaning to a button",
+        date : 1421153200637
+    },{
+        url: "gallary/MyHobbi/81VibVp501o.jpg",
+        name: "НАЗВАНИЕ",
+        id : 3,
+        description : " be conveyed to users of assistive technologies",
+        date : 1426153200637
+    },{
+        url: "gallary/MyHobbi/AQuoNZumPNI.jpg",
+        name: "ОПИСАНИЕ",
+        id : 4,
+        description : "ssistive technologies – such as screen readers. Ensure",
+        date : 1428153200637
+    },{
+        url: "gallary/MyHobbi/oHvDjUX8Z4A.jpg",
+        name: "ИМЯ МАШИНЫ",
+        id : 5,
+        description : "color to add meaning to a button only provides",
+        date : 1402153200637
+    },{
+        url: "gallary/MyHobbi/WhaIf66UWHg.jpg",
+        name: "МЕРСЕДЕС",
+        id : 6,
+        description : "om the content itself (the visible text of the button)",
+        date : 1442153200637
+    }],
+
+    photo18 : [{
+        url: "gallary/18/1951_320_n_store.jpg",
+        name: "МАШИНКА",
+        id : 1,
+        description : " be conveyed to users of assistive technologies – such as",
+        date : 1422153200637
+    },{
+        url: "gallary/18/3968_500_n_store.jpg",
+        name: "ДРУГАЯ МАШИНКА",
+        id : 2,
+        description : "sing color to add meaning to a button",
+        date : 1421153200637
+    },{
+        url: "gallary/18/53123e973ed4f.jpg",
+        name: "НАЗВАНИЕ",
+        id : 3,
+        description : " be conveyed to users of assistive technologies",
+        date : 1426153200637
+    },{
+        url: "gallary/18/534270f1afb6d.jpg",
+        name: "ОПИСАНИЕ",
+        id : 4,
+        description : "ssistive technologies – such as screen readers. Ensure",
+        date : 1428153200637
+    },{
+        url: "gallary/18/587281.jpg",
+        name: "ИМЯ МАШИНЫ",
+        id : 5,
+        description : "color to add meaning to a button only provides",
+        date : 1402153200637
+    },{
+        url: "gallary/18/735594.jpg",
+        name: "МЕРСЕДЕС",
+        id : 6,
+        description : "om the content itself (the visible text of the button)",
+        date : 1442153200637
+    },{
+        url: "gallary/18/771100.jpg",
+        name: "БМВ",
+        id : 7,
+        description : "r is either obvious from the content itself",
+        date : 1482153200637
+    },{
+        url: "gallary/18/817462.jpg",
+        name: "ЛАДА КАЛИНА",
+        id : 8,
+        description : "included through alternative means, such as additional text hidden with the",
+        date : 1442153200637
+    }, {
+        url: "gallary/18/927487.jpg",
+        name: "ДАЧА ЛОГАН",
+        id: 10,
+        description: "uded through alternative means, such as additional",
+        date: 1322159200637
+    },{
+        url: "gallary/18/934075.jpg",
+        name: "МАШИНКА",
+        id : 11,
+        description : " be conveyed to users of assistive technologies – such as",
+        date : 1422153200637
+    },{
+        url: "gallary/18/948342.jpg",
+        name: "ДРУГАЯ МАШИНКА",
+        id : 12,
+        description : "sing color to add meaning to a button",
+        date : 1421153200637
+    },{
+        url: "gallary/18/2293472.jpg",
+        name: "НАЗВАНИЕ",
+        id : 13,
+        description : " be conveyed to users of assistive technologies",
+        date : 1426153200637
+    },{
+        url: "gallary/18/GDLmcFNEOIY.jpg",
+        name: "ОПИСАНИЕ",
+        id : 14,
+        description : "ssistive technologies – such as screen readers. Ensure",
+        date : 1428153200637
+    },{
+        url: "gallary/18/IgdgMWEq6po.jpg",
+        name: "ИМЯ МАШИНЫ",
+        id : 15,
+        description : "color to add meaning to a button only provides",
+        date : 1402153200637
+    },{
+        url: "gallary/18/uv4PmzsY0uc.jpg",
+        name: "МЕРСЕДЕС",
+        id : 16,
+        description : "om the content itself (the visible text of the button)",
+        date : 1442153200637
+    }],
+
+    cars : [{
+        url: "http://desktopwallpapers.org.ua/mini/201507/40069.jpg",
+        name: "МАШИНКА",
+        id : 1,
+        description : " be conveyed to users of assistive technologies – such as",
+        date : 1422153200637
+    },{
+        url: "http://desktopwallpapers.org.ua/mini/201507/40068.jpg",
+        name: "ДРУГАЯ МАШИНКА",
+        id : 2,
+        description : "sing color to add meaning to a button",
+        date : 1421153200637
+    },{
+        url: "http://desktopwallpapers.org.ua/mini/201507/40067.jpg",
+        name: "НАЗВАНИЕ",
+        id : 3,
+        description : " be conveyed to users of assistive technologies",
+        date : 1426153200637
+    },{
+        url: "http://desktopwallpapers.org.ua/mini/201507/40057.jpg",
+        name: "ОПИСАНИЕ",
+        id : 4,
+        description : "ssistive technologies – such as screen readers. Ensure",
+        date : 1428153200637
+    },{
+        url: "http://desktopwallpapers.org.ua/mini/201507/40066.jpg",
+        name: "ИМЯ МАШИНЫ",
+        id : 5,
+        description : "color to add meaning to a button only provides",
+        date : 1402153200637
+    },{
+        url: "http://desktopwallpapers.org.ua/mini/201507/40064.jpg",
+        name: "МЕРСЕДЕС",
+        id : 6,
+        description : "om the content itself (the visible text of the button)",
+        date : 1442153200637
+    },{
+        url: "http://desktopwallpapers.org.ua/mini/201507/40063.jpg",
+        name: "БМВ",
+        id : 7,
+        description : "r is either obvious from the content itself",
+        date : 1482153200637
+    },{
+        url: "http://desktopwallpapers.org.ua/mini/201507/40062.jpg",
+        name: "ЛАДА КАЛИНА",
+        id : 8,
+        description : "included through alternative means, such as additional text hidden with the",
+        date : 1442153200637
+    },{
+        url: "http://desktopwallpapers.org.ua/mini/201507/40059.jpg",
+        name: "ДЖИП",
+        id : 9,
+        description : " meaning to a button only provides a visual",
+        date : 1322153200637
+    },{
+        url: "http://desktopwallpapers.org.ua/mini/201507/40058.jpg",
+        name: "ДАЧА ЛОГАН",
+        id : 10,
+        description : "uded through alternative means, such as additional",
+        date : 1322159200637
+    }],
+
+
+    chernivtsi : [{
+        url: "gallary/Chernivtsi/AxQcrv1QkZ4.jpg",
+        name: "МАШИНКА",
+        id : 1,
+        description : " be conveyed to users of assistive technologies – such as",
+        date : 1422153200637
+    },{
+        url: "gallary/Chernivtsi/InDZTNjQ7IE.jpg",
+        name: "ДРУГАЯ МАШИНКА",
+        id : 2,
+        description : "sing color to add meaning to a button",
+        date : 1421153200637
+    },{
+        url: "gallary/Chernivtsi/Kp_Z_329wvI.jpg",
+        name: "НАЗВАНИЕ",
+        id : 3,
+        description : " be conveyed to users of assistive technologies",
+        date : 1426153200637
+    },{
+        url: "gallary/Chernivtsi/KPSW6n8ywk8.jpg",
+        name: "ОПИСАНИЕ",
+        id : 4,
+        description : "ssistive technologies – such as screen readers. Ensure",
+        date : 1428153200637
+    },{
+        url: "gallary/Chernivtsi/v75DRMED9w4.jpg",
+        name: "ИМЯ МАШИНЫ",
+        id : 5,
+        description : "color to add meaning to a button only provides",
+        date : 1402153200637
+    },{
+        url: "gallary/Chernivtsi/x_38a9bc0e.jpg",
+        name: "МЕРСЕДЕС",
+        id : 6,
+        description : "om the content itself (the visible text of the button)",
+        date : 1442153200637
+    },{
+        url: "gallary/Chernivtsi/x_79694b52.jpg",
+        name: "БМВ",
+        id : 7,
+        description : "r is either obvious from the content itself",
+        date : 1482153200637
+    },{
+        url: "gallary/Chernivtsi/x_b2d01839.jpg",
+        name: "ЛАДА КАЛИНА",
+        id : 8,
+        description : "included through alternative means, such as additional text hidden with the",
+        date : 1442153200637
+    },{
+        url: "gallary/Chernivtsi/x_f9e786ef.jpg",
+        name: "ДЖИП",
+        id : 9,
+        description : " meaning to a button only provides a visual",
+        date : 1322153200637
+    }, {
+        url: "gallary/Chernivtsi/x_f41fb576.jpg",
+        name: "ДАЧА ЛОГАН",
+        id: 10,
+        description: "uded through alternative means, such as additional",
+        date: 1322159200637
+    },{
+        url: "gallary/Chernivtsi/z_67afa680.jpg",
+        name: "МАШИНКА",
+        id : 11,
+        description : " be conveyed to users of assistive technologies – such as",
+        date : 1422153200637
+    },{
+        url: "gallary/Chernivtsi/z_718965ec.jpg",
+        name: "ДРУГАЯ МАШИНКА",
+        id : 12,
+        description : "sing color to add meaning to a button",
+        date : 1421153200637
+    },{
+        url: "gallary/Chernivtsi/z_86887314.jpg",
+        name: "НАЗВАНИЕ",
+        id : 13,
+        description : " be conveyed to users of assistive technologies",
+        date : 1426153200637
+    },{
+        url: "gallary/Chernivtsi/z_bf556bee.jpg",
+        name: "ОПИСАНИЕ",
+        id : 14,
+        description : "ssistive technologies – such as screen readers. Ensure",
+        date : 1428153200637
+    },{
+        url: "gallary/Chernivtsi/z_d3af8f34.jpg",
+        name: "ИМЯ МАШИНЫ",
+        id : 15,
+        description : "color to add meaning to a button only provides",
+        date : 1402153200637
+    },{
+        url: "gallary/Chernivtsi/z_d442cfe9.jpg",
+        name: "МЕРСЕДЕС",
+        id : 16,
+        description : "om the content itself (the visible text of the button)",
+        date : 1442153200637
+    }],
+
+    girls : [{
+        url: "gallary/girls/2_frNozSzPI.jpg",
+        name: "МАШИНКА",
+        id : 1,
+        description : " be conveyed to users of assistive technologies – such as",
+        date : 1422153200637
+    },{
+        url: "gallary/girls/2fwEChaAQwY.jpg",
+        name: "ДРУГАЯ МАШИНКА",
+        id : 2,
+        description : "sing color to add meaning to a button",
+        date : 1421153200637
+    },{
+        url: "gallary/girls/2_frNozSzPI.jpg",
+        name: "НАЗВАНИЕ",
+        id : 3,
+        description : " be conveyed to users of assistive technologies",
+        date : 1426153200637
+    },{
+        url: "gallary/girls/5agJey-Lzm4.jpg",
+        name: "ОПИСАНИЕ",
+        id : 4,
+        description : "ssistive technologies – such as screen readers. Ensure",
+        date : 1428153200637
+    },{
+        url: "gallary/girls/7ub_m_GRboc.jpg",
+        name: "ИМЯ МАШИНЫ",
+        id : 5,
+        description : "color to add meaning to a button only provides",
+        date : 1402153200637
+    },{
+        url: "gallary/girls/45M3oyIfheA.jpg",
+        name: "МЕРСЕДЕС",
+        id : 6,
+        description : "om the content itself (the visible text of the button)",
+        date : 1442153200637
+    },{
+        url: "gallary/girls/66zCSp9FIbI.jpg",
+        name: "БМВ",
+        id : 7,
+        description : "r is either obvious from the content itself",
+        date : 1482153200637
+    },{
+        url: "gallary/girls/A7oUBov0zq4.jpg",
+        name: "ЛАДА КАЛИНА",
+        id : 8,
+        description : "included through alternative means, such as additional text hidden with the",
+        date : 1442153200637
+    },{
+        url: "gallary/girls/H7zmBzleNms.jpg",
+        name: "ДЖИП",
+        id : 9,
+        description : " meaning to a button only provides a visual",
+        date : 1322153200637
+    }, {
+        url: "gallary/girls/I8j8ILO3xWg.jpg",
+        name: "ДАЧА ЛОГАН",
+        id: 10,
+        description: "uded through alternative means, such as additional",
+        date: 1322159200637
+    },{
+        url: "gallary/girls/iIcHZU17JE0.jpg",
+        name: "МАШИНКА",
+        id : 11,
+        description : " be conveyed to users of assistive technologies – such as",
+        date : 1422153200637
+    },{
+        url: "gallary/girls/KWNmOt-L164.jpg",
+        name: "ДРУГАЯ МАШИНКА",
+        id : 12,
+        description : "sing color to add meaning to a button",
+        date : 1421153200637
+    },{
+        url: "gallary/girls/mFflOj64U-E.jpg",
+        name: "НАЗВАНИЕ",
+        id : 13,
+        description : " be conveyed to users of assistive technologies",
+        date : 1426153200637
+    },{
+        url: "gallary/girls/NxktSlcXCqo.jpg",
+        name: "ОПИСАНИЕ",
+        id : 14,
+        description : "ssistive technologies – such as screen readers. Ensure",
+        date : 1428153200637
+    },{
+        url: "gallary/girls/OetpzEirW-Q.jpg",
+        name: "ИМЯ МАШИНЫ",
+        id : 15,
+        description : "color to add meaning to a button only provides",
+        date : 1402153200637
+    },{
+        url: "gallary/girls/skEA2RTNFIU.jpg",
+        name: "МЕРСЕДЕС",
+        id : 16,
+        description : "om the content itself (the visible text of the button)",
+        date : 1442153200637
+    },{
+        url: "gallary/girls/tJvDsUBPIhU.jpg",
+        name: "БМВ",
+        id : 17,
+        description : "r is either obvious from the content itself",
+        date : 1482153200637
+    },{
+        url: "gallary/girls/WHHM18lsvcs.jpg",
+        name: "ЛАДА КАЛИНА",
+        id : 18,
+        description : "included through alternative means, such as additional text hidden with the",
+        date : 1442153200637
+    },{
+        url: "gallary/girls/z_27dd528b.jpg",
+        name: "ДЖИП",
+        id : 19,
+        description : " meaning to a button only provides a visual",
+        date : 1322153200637
+    },{
+        url: "gallary/girls/zUanDPFwkQQ.jpg",
+        name: "ДАЧА ЛОГАН",
+        id : 20,
+        description : "uded through alternative means, such as additional",
+        date : 1322159200637
+    }],
+
+    mountains : [{
+        url: "gallary/Mountains/EQTPToyG3VY.jpg",
+        name: "МАШИНКА",
+        id : 1,
+        description : " be conveyed to users of assistive technologies – such as",
+        date : 1422153200637
+    },{
+        url: "gallary/Mountains/KmeO58RxXss.jpg",
+        name: "ДРУГАЯ МАШИНКА",
+        id : 2,
+        description : "sing color to add meaning to a button",
+        date : 1421153200637
+    },{
+        url: "gallary/Mountains/Uo-hFzoZiZg.jpg",
+        name: "НАЗВАНИЕ",
+        id : 3,
+        description : " be conveyed to users of assistive technologies",
+        date : 1426153200637
+    },{
+        url: "gallary/Mountains/x_990e2e1c.jpg",
+        name: "ИМЯ МАШИНЫ",
+        id : 5,
+        description : "color to add meaning to a button only provides",
+        date : 1402153200637
+    },{
+        url: "gallary/Mountains/x_c0615a9c.jpg",
+        name: "МЕРСЕДЕС",
+        id : 6,
+        description : "om the content itself (the visible text of the button)",
+        date : 1442153200637
+    },{
+        url: "gallary/Mountains/x_f713c2a1.jpg",
+        name: "БМВ",
+        id : 7,
+        description : "r is either obvious from the content itself",
+        date : 1482153200637
+    },{
+        url: "gallary/Mountains/z_2cd60b3f.jpg",
+        name: "ЛАДА КАЛИНА",
+        id : 8,
+        description : "included through alternative means, such as additional text hidden with the",
+        date : 1442153200637
+    },{
+        url: "gallary/Mountains/z_055e3f57.jpg",
+        name: "ДЖИП",
+        id : 9,
+        description : " meaning to a button only provides a visual",
+        date : 1322153200637
+    }, {
+        url: "gallary/Mountains/z_56fd9dec.jpg",
+        name: "ДАЧА ЛОГАН",
+        id: 10,
+        description: "uded through alternative means, such as additional",
+        date: 1322159200637
+    },{
+        url: "gallary/Mountains/z_65924ef9.jpg",
+        name: "МАШИНКА",
+        id : 11,
+        description : " be conveyed to users of assistive technologies – such as",
+        date : 1422153200637
+    },{
+        url: "gallary/Mountains/z_d97eea7b.jpg",
+        name: "ДРУГАЯ МАШИНКА",
+        id : 12,
+        description : "sing color to add meaning to a button",
+        date : 1421153200637
+    }]
+
+};
+
+
+
