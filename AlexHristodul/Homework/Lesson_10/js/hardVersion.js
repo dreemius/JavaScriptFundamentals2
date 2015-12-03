ecd3a61cbfbce95743dfab251377f61242a792e8
AlexHristodul <AlexHristodul@gmail.com>
2015-12-03 18:47:26 +0200

    HW_10

--- AlexHristodul/Homework/Lesson_10/js/hardVersion.js
+++ AlexHristodul/Homework/Lesson_10/js/hardVersion.js
@@ -1,45 +1,79 @@
 'use strict';
-// Created by iPhoenix© development 02.12.2015
+// Created by iPhoenix© development 03.12.2015
 //********************************************
 
 var kettlerHandler = (function() {
+    var input = null;
+
     function TeaPot(name) {
         this.isBoiling = false;
         this.name = name;
     }
 
     TeaPot.prototype = {
-        turnOn : function () {
+        turnOn : function() {
             this.isBoiling = true;
-            console.log('Чайник для заварки ' + this.name + ' включен.');
+            input.kettleOn.html('<br/>' + 'Чайник по имени: "' + this.name + '" - включен.');
+            showBanner(input.kettleOn);
         },
-        turnOff : function () {
+        turnOff : function() {
             this.isBoiling = false;
-            console.log('Чайник для заварки ' + this.name + ' выключен.');
+            input.kettleOff.html('<br/>' + 'Чайник по имени: "' + this.name + '" - выключен.');
+            showBanner(input.kettleOff);
         }
     };
 
-    function ElectroTeaPot(name) {
+    function ElectroKettle(name) {
         TeaPot.call(this, name);
     }
 
-    ElectroTeaPot.prototype = {
-        turnOn : function () {
-            console.log('Кнопка нажата');
+    ElectroKettle.prototype = {
+        turnOn : function() {
+            input.kettleStat.html('<br/>' + 'Кнопка нажата');
             TeaPot.prototype.turnOn.apply(this, arguments);
         }
     };
 
-    inheritense(TeaPot, ElectroTeaPot);
-
-    function printPot() {
-        var electroTeaPot = new ElectroTeaPot("Xiaxuya");
-        console.dir(electroTeaPot);
-        electroTeaPot.turnOn();
-        electroTeaPot.turnOff();
+    function SunKettle(name) {
+        ElectroKettle.call(this, name);
     }
 
+    SunKettle.prototype = {
+        turnOn : function() {
+            TeaPot.prototype.turnOn.apply(this, arguments);
+            input.kettleStat.html('<br/>' + 'Пошла зарядка');
+            showBanner(input.kettleStat);
+        }
+    };
+
+    inheritense(TeaPot, ElectroKettle);
+    inheritense(ElectroKettle, SunKettle);
+
+    function showBanner(selector) {
+        selector.fadeIn(3250, function () {
+            $(this).fadeOut(3250);
+        });
+    }
+
+    function addKettleName() {
+        input.nameOfKettle.html('"' + input.inputName.val() + '"');
+    }
+
+    function addKettle() {
+        var sunKettle = new SunKettle(input.inputName.val());
+        input.formGroup.hide();
+        addKettleName();
+        sunKettle.turnOn();
+        sunKettle.turnOff();
+    }
+
+    function printPot() {
+        input.successButton.click(addKettle);
+    }
     return {
+        setKettler: function(teaPots) {
+            input = teaPots;
+        },
         init: function() {
             printPot()
         }
@@ -47,5 +81,16 @@ var kettlerHandler = (function() {
 }());
 
 //*********************************************
+
+kettlerHandler.setKettler({
+    inputName    : $('#inputName'),
+    checkKettle  : $('#checkKettle'),
+    nameOfKettle : $('.name-of-kettle'),
+    successButton: $('.btn-success'),
+    formGroup    : $('.form-group'),
+    kettleOn     : $('#kettle-on'),
+    kettleOff    : $('#kettle-off'),
+    kettleStat   : $('#kettle-stat')
+});
 
 kettlerHandler.init();
