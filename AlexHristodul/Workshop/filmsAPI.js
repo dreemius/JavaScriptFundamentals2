1a1b34d1ac1fc92166c7bec7feb447fda35b61ba
AlexHristodul <AlexHristodul@gmail.com>
2015-12-12 13:06:26 +0200

    WorkShop

---
+++ AlexHristodul/Workshop/filmsAPI.js
@@ -0,0 +1,34 @@
+var films = (function(){
+
+	window.onDataLoaded = function(list) {
+		console.dir(list);
+	};
+
+	function search (searchString){
+		jQuery.getScript('http://www.omdbapi.com/?s='+searchString+'&plot=short&r=json&callback=onDataLoaded');
+	}
+
+	return {
+		search : function(searchString){
+			search (searchString);
+		},
+		getFilmInfo: function(form){
+
+		}
+	}
+
+
+}());
+
+films.search('house');
+
+
+/*
+ jQuery.ajax({
+ url: 'http://sg.media-imdb.com/suggests/s/star.json',
+ dataType: 'jsonp',
+ cache: true,
+ jsonp: false,
+ jsonpCallback: 'imdb$star'
+ }).done(function (result) {});
+ */
---
+++ AlexHristodul/Workshop/gallery.js
@@ -0,0 +1,68 @@
+
+var filmLoader = (function(){
+	var resultContainer = $('#result');
+	var compiled = _.template('<div class="col-xs-6">\
+				<img src="<%=url%>" alt="<%=name%>" class="img-thumbnail pull-left">\
+				<div class="info-wrapper pull-left">\
+					<div class="text-muted"><%=description%></div>\
+				</div>\
+			</div>');
+
+
+	function buildOneItem() {
+		var item = {
+			url: "http://desktopwallpapers.org.ua/mini/201507/40066.jpg",
+			name: "картинка 1",
+			description : "Using color to add meaning only"
+		};
+
+		var html = compiled({
+			url: item.url,
+			name: item.name,
+			description: item.description
+		});
+		resultContainer.html(html);
+	}
+
+
+	function getQueryString() {
+		var searchInput = $('#search').val();
+		console.log(searchInput);
+		return searchInput;
+	}
+
+	//don't change
+	function search (searchString){
+		jQuery.getScript('http://www.omdbapi.com/?s='+searchString+'&plot=short&r=json&callback=onDataLoaded');
+	}
+
+
+	function buildTable (data) {
+		console.log(data);
+		buildOneItem();
+	}
+
+
+	function init (){
+		$('#btnSearch').on('click', function() {
+				search(getQueryString());
+				buildTable();
+			});
+	}
+	init();
+
+	return {
+		//don't change
+		buildTable : function(data) {
+			buildTable(data);
+		}
+	}
+
+}());
+
+//don't change
+window.onDataLoaded = function(list) {
+	filmLoader.buildTable(list);
+};
+
+
