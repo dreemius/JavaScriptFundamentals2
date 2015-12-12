7752bb998266cabcf3b2779957557a9489c0fa67
Eugene <dreemius@gmail.com>
2015-12-12 11:14:09 +0200

    workshop2

--- Dreemius/workshop/gallery.js
+++ Dreemius/workshop/gallery.js
@@ -1,56 +1,21 @@
 
-
-/*
-var item = {
-	url: "http://desktopwallpapers.org.ua/mini/201507/40066.jpg",
-	name: "картинка 1",
-	id : 1,
-	description : "Using color to add meaning only",
-	date : 1422153200637
-}
-*/
-
-/*
-var resultContainer = $('#result');
-var resultHTML = "";
-var itemTemplate = '<div class="col-xs-6">\
-				<img src="$url" alt="$name" class="img-thumbnail pull-left">\
-				<div class="info-wrapper pull-left">\
-					<div class="text-muted">$number: $name</div>\
-					<div class="text-muted">$description</div>\
-					<div class="text-muted">$date</div>\
-				</div>\
-			</div>';
-
-resultHTML += itemTemplate
-	.replace("$number", item.id)
-	.replace(/\$name/gi, item.name)
-	.replace("$url", item.url)
-	.replace("$description", item.description)
-	.replace("$date", item.date);
-			
-resultContainer.html(resultHTML);
-
-
-*/
-
 var filmLoader = (function(){
 	var resultContainer = $('#result');
+	var compiled = _.template('<div class="col-xs-6">\
+				<img src="<%=url%>" alt="<%=name%>" class="img-thumbnail pull-left">\
+				<div class="info-wrapper pull-left">\
+					<div class="text-muted"><%=description%></div>\
+				</div>\
+			</div>');
 
 
 	function buildOneItem() {
 		var item = {
 			url: "http://desktopwallpapers.org.ua/mini/201507/40066.jpg",
 			name: "картинка 1",
-			description : "Using color to add meaning only",
+			description : "Using color to add meaning only"
 		};
 
-		var compiled = _.template('<div class="col-xs-6">\
-				<img src="<%=url%>" alt="<%=name%>" class="img-thumbnail pull-left">\
-				<div class="info-wrapper pull-left">\
-					<div class="text-muted"><%=description%></div>\
-				</div>\
-			</div>');
 		var html = compiled({
 			url: item.url,
 			name: item.name,
@@ -64,6 +29,7 @@ var filmLoader = (function(){
 		return "star";
 	}
 
+	//don't change
 	function search (searchString){
 		jQuery.getScript('http://www.omdbapi.com/?s='+searchString+'&plot=short&r=json&callback=onDataLoaded');
 	}
@@ -77,10 +43,12 @@ var filmLoader = (function(){
 
 	function init (){
 		search (getQueryString());
+
 	}
 	init();
 
 	return {
+		//don't change
 		buildTable : function(data) {
 			buildTable(data);
 		}
@@ -88,6 +56,7 @@ var filmLoader = (function(){
 
 }());
 
+//don't change
 window.onDataLoaded = function(list) {
 	filmLoader.buildTable(list);
 };
