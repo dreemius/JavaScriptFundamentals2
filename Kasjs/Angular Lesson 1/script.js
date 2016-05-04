bf1e01039359d3cf5a0307a591d387c7a66da987
Kasjs <gemyni85@gmail.com>
2016-05-04 23:02:38 +0300

    angular

--- Kasjs/Angular Lesson 1/script.js
+++ Kasjs/Angular Lesson 1/script.js
@@ -1,1 +1,31 @@
+var myApp = angular.module('App', []);
+myApp.controller('userListCtrl', ['$scope', function($scope) {
+    $scope.list = [];
 
+    function addItem() {
+        if ($scope.name && $scope.pass && $scope.word) {
+            $scope.list.push({
+                id: new Date().getTime(),
+                name: $scope.name,
+                pass: $scope.pass,
+                word: $scope.word
+            });
+        }
+    }
+
+    function removeItem(id) {
+        angular.forEach($scope.list, function(val, key) {
+            if (val.id == id) {
+                $scope.list.splice(key, 1);
+            }
+        })
+    }
+
+    $scope.add = function() {
+        addItem();
+    };
+    $scope.remove = function(id) {
+        removeItem(id);
+    };
+}]);
+myApp.run();
